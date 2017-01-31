b07143030c9e0427b18b7f8eefaa170d30081f02
p4suhag <p4suhag@gmail.com>
2017-01-31 11:44:19 +0530

    Style the card

--- app/components/cardscontainer/mydiet/mydietview.jsx
+++ app/components/cardscontainer/mydiet/mydietview.jsx
@@ -11,29 +11,29 @@ const Mydietview = React.createClass({
                     <div className="card__content">
                         <p className="card__name">{this.props.nutrition.name}</p>
                         <div className="pure-g">
-                            <div className="pure-u-1-3 pure-u-md-1-3 pure-u-lg-1-3">
+                            <div className="card__nutrition">
                                 <p>Name</p>
                                 <p>Calories</p>
                                 <p>Protiens</p>
                                 <p>Carbs</p>
                                 <p>Fat</p>
                             </div>
-                            <div className="pure-u-1-3 pure-u-md-1-3 pure-u-lg-1-3">
+                            <div className="card__nutrition pad__lft">
                                 <p>Norms</p>
-                                <p>{this.props.nutrition.calories.amount}</p>
-                                <p>{this.props.nutrition.protein.amount}</p>
-                                <p>{this.props.nutrition.carbs.amount}</p>
-                                <p>{this.props.nutrition.fat.amount}</p>
+                                <p>{Math.ceil(this.props.nutrition.calories.amount)}g</p>
+                                <p>{Math.ceil(this.props.nutrition.protein.amount)}g</p>
+                                <p>{Math.ceil(this.props.nutrition.carbs.amount)}g</p>
+                                <p>{Math.ceil(this.props.nutrition.fat.amount)}g</p>
                             </div>
-                            <div className="pure-u-1-3 pure-u-md-1-3 pure-u-lg-1-3">
+                            <div className="card__nutrition pad__lft">
                                 <p>Percent</p>
-                                <p>{this.props.nutrition.calories.percent}</p>
-                                <p>{this.props.nutrition.protein.percent}</p>
-                                <p>{this.props.nutrition.carbs.percent}</p>
-                                <p>{this.props.nutrition.fat.percent}</p>
+                                <p>{Math.ceil(this.props.nutrition.calories.percent)} %</p>
+                                <p>{Math.ceil(this.props.nutrition.protein.percent)} %</p>
+                                <p>{Math.ceil(this.props.nutrition.carbs.percent)} %</p>
+                                <p>{Math.ceil(this.props.nutrition.fat.percent)} %</p>
                             </div>
                         </div>
-                        <p className="card__button" onClick={this.props.removecard(this.props.nutrition.id)} >Remove</p>                 
+                        <p className="card__button dietbtn" onClick={this.props.removecard(this.props.nutrition.id)} >Remove</p>                 
                     </div>
                 </div>
             </div>
